1b63719f1ddd2774260d3ab613c2197386d342ac
David Stensland <me@terite.com>
2018-07-31 15:05:40 -0700

    closer types from json

--- src/game.ts
+++ src/game.ts
@@ -6,46 +6,105 @@ import {assert} from "./util"
 type LocalisedName = {[locale: string]: string}
 
 namespace JsonData {
+
+    interface Effectable {
+        // TODO: export empty allowed_effects
+        allowed_effects?: Array<"consumption" | "pollution" | "productivity" | "speed">
+        module_slots: number
+    }
+
     interface BaseEntity {
         name: string;
         localised_name: LocalisedName;
         icon_col: number;
         icon_row: number;
+
+        energy_usage?: number;
+        energy_source?: {
+            fuel_category: string
+            type: "burner"
+        }
     };
 
-    type EntityMap = {[name: string]: BaseEntity}
+    interface AssemblingMachine extends BaseEntity, Effectable {
+        crafting_categories: string[]
+        crafting_speed: number
+        ingredient_count: number
+    }
 
-    export interface Item {
+    interface MiningDrill extends BaseEntity, Effectable {
+        mining_power: number;
+        mining_speed: number;
+        resource_categories: string[];
+    }
+
+    interface OffshorePump extends BaseEntity {
+        fluid: string
+    }
+
+    interface RocketSilo extends AssemblingMachine, Effectable {
+        rocket_parts_required: number;
+    }
+
+    interface TransportBelt extends BaseEntity {
+        belt_speed: number
+    }
+
+    interface BaseItem {
         name: string;
         localised_name: LocalisedName;
         icon_col: number;
         icon_row: number;
+        group: string;
+        subgroup: string
+        order: string;
+        type: string;
+
+        // rocket_launch_products: Result[]  // TODO: collect
     }
+
+    interface FuelItem extends BaseItem {
+        fuel_category: string;
+        fuel_value: number;
+    }
+
+    interface ModuleItem extends BaseItem {
+        category: "effectivity" | "productivity" | "speed"
+        effect: {
+            consumption?: {bonus: number}
+            pollution?: {bonus: number}
+            productivity?: {bonus: number}
+            speed?: {bonus: number}
+        }
+        limitation: string[]
+    }
+
+    export type Item = BaseItem | FuelItem | ModuleItem
 
     export interface Ingredient {
         name: string
         amount: number
-        type?: "item" | "fluid"
+        type?: "item" | "fluid" // TODO: always export type
         minimum_temperature?: number
         maximum_temperature?: number
     }
 
-    interface BaseResult {
+    interface BaseProduct {
         name: string
-        type?: "item" | "fluid"
+        type?: "item" | "fluid" // TODO: always export type
         temperature?: number
         probability?: number
     }
 
-    interface ResultRange extends BaseResult {
+    interface ProductRange extends BaseProduct {
         amount_min: number
         amount_max: number
     }
-    interface ResultAmount extends BaseResult {
+    interface ProductAmount extends BaseProduct {
         amount: number
     }
 
-    export type Result = ResultAmount | ResultRange;
+    export type Product = ProductAmount | ProductRange;
 
     export interface Recipe {
         name: string;
@@ -57,31 +116,60 @@ namespace JsonData {
         icon_col: number;
         icon_row: number;
         ingredients: Ingredient[];
-        results: Result[];
+        results: Product[]; // TODO: rename results -> products
         type: "recipe";
+        order: string;
+
+        requester_paste_multiplier?: number // TODO: remove
+        main_product?: string
+    }
+
+    export interface Resource {
+        name: string;
+        localised_name: LocalisedName;
+        icon_col: number;
+        icon_row: number;
+
+        category: string;
+
+        minable: {
+            hardness: number;
+            mining_time: number;
+            mining_particle?: string;  // TODO: remove
+            results: Product[]
+            fluid_amount?: number
+            required_fluid?: string
+        }
+
     }
 
     export interface Root {
+        active_mods: {[name: string]: string};
+
+        groups: {[groupName: string]: {
+            order: string
+            subgroups: {[subName: string]: string}
+        }}
+
+
         fluids: string[];
         fuel: string[];
         modules: string[];
 
         recipes: {[name: string]: Recipe};
-        resource: TODO;
+        resource: {[name: string]: Resource};
         sprites: TODO;
 
         items: {[name: string]: Item};
 
         // other entities
-        "accumulator": EntityMap;
-        "assembling-machine": EntityMap;
-        "boiler": EntityMap;
-        "generator": EntityMap;
-        "mining-drill": EntityMap;
-        "offshore-pump": EntityMap;
-        "reactor": EntityMap;
-        "rocket-silo": EntityMap;
-        "solar-panel": EntityMap;
+        "assembling-machine": {[name: string]: AssemblingMachine};
+        "furnace": {[name: string]: AssemblingMachine};
+        "mining-drill": {[name: string]: MiningDrill};
+        "offshore-pump": {[name: string]: OffshorePump};
+        "reactor": {[name: string]: BaseEntity};
+        "rocket-silo": {[name: string]: RocketSilo};
+        "transport-belt": {[name: string]: TransportBelt}
     }
 }
 
@@ -128,7 +216,7 @@ export class BaseIngredient {
     name: string;
     item: Item;
     
-    constructor(d: JsonData.Ingredient | JsonData.Result, gd: GameData) {
+    constructor(d: JsonData.Ingredient | JsonData.Product, gd: GameData) {
         this.type = d.type || "item";
         this.name = d.name;
         this.item = gd.itemMap[d.name]
@@ -153,7 +241,7 @@ export class Product extends BaseIngredient {
     temperature?: number;
     amount: Rational
 
-    constructor(d: JsonData.Result, gd: GameData) {
+    constructor(d: JsonData.Product, gd: GameData) {
         super(d, gd)
         this.temperature = d.temperature
 
