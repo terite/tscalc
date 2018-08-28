79dfb579e934c0869a82365f3b96e7be4adb78ab
David Stensland <me@terite.com>
2018-08-28 16:31:31 -0700

    remove unused well element

--- src/components/generic/Well.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react"
-
-export class UnusedWell extends React.Component<React.HTMLAttributes<HTMLDivElement>, {}> {
-    render () {
-        return <div className="well" {...this.props} />
-    }
-}
--- src/components/generic/index.tsx
+++ src/components/generic/index.tsx
@@ -1,3 +1,2 @@
 export {Dropdown} from './Dropdown'
-export {UnusedWell} from './Well'
 export {PopperHelper} from './PopperHelper'
